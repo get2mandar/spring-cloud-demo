0ddac439fa26b94fcfcc16a9ec4303058e32fc8b
Mandar Pandit <panditm@pmc.local>
2022-06-02 20:07:52 +0530

    Gateway Working Hurraygit status and Zipkin with Sleuth Added

--- apps-api/book-catalog-service/src/main/java/blog/panditmandar/scdemo/bookcatalog/BookCatalogController.java
+++ apps-api/book-catalog-service/src/main/java/blog/panditmandar/scdemo/bookcatalog/BookCatalogController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/catalog")
 public class BookCatalogController {
 
 	@Autowired
