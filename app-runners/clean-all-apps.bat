5051d6f501662a5288b04ad2e4a936074e2fc483
Mandar Pandit <panditm@pmc.local>
2022-09-30 16:43:55 +0530

    ConfigStore moved to separate Repo - Added again Config Base Dir - Postman Files Updated for EDGE - Maven Clean BatFileAdded

---
+++ app-runners/clean-all-apps.bat
@@ -0,0 +1,63 @@
+@ECHO OFF
+:BEGIN
+CLS
+
+TITLE CLEAN-ALL-APPS - MAVEN CLEAN
+
+SET command=mvn clean
+
+ECHO Clean Infra Services
+
+CD ../apps-infra
+
+ECHO ------------
+ECHO config-server
+ECHO ------------
+CD config-server
+CALL %command%
+
+
+ECHO ------------
+ECHO eureka-server
+ECHO ------------
+CD ../eureka-server
+CALL %command%
+
+
+ECHO ------------
+ECHO edge-gateway
+ECHO ------------
+CD ../edge-gateway
+CALL %command%
+
+
+ECHO .
+timeout /T 5 /NOBREAK>nul
+ECHO .
+
+ECHO Clean API Services
+
+CD ../../apps-api/
+
+ECHO ------------
+ECHO book-data-service
+ECHO ------------
+CD book-data-service
+CALL %command%
+
+
+ECHO ------------
+ECHO book-added-info-service
+ECHO ------------
+CD ../book-added-info-service
+CALL %command%
+
+
+ECHO ------------
+ECHO book-catalog-service
+ECHO ------------
+CD ../book-catalog-service
+CALL %command%
+
+:END
+pause
--- app-runners/run-files/run-infra-config-server.bat
+++ app-runners/run-files/run-infra-config-server.bat
@@ -7,7 +7,7 @@ TITLE RUN config-server 8888 git-default
 
 CD ../../apps-infra/config-server
 
-CALL java -jar -XX:+UseSerialGC -Xss512k target\config-server-0.1.jar --spring.cloud.config.server.git.uri=%GH_SCDEMO_URI% --spring.cloud.config.server.git.username=%GH_USER% --spring.cloud.config.server.git.password=%GHP_RONLY_TOKEN%
+CALL java -jar -XX:+UseSerialGC -Xss512k target\config-server-0.1.jar --spring.cloud.config.server.git.uri=%GH_SCDEMO_URI% --spring.cloud.config.server.git.username=%GH_USER% --spring.cloud.config.server.git.password=%GHP_RONLY_TOKEN%  --spring.cloud.config.server.git.basedir=%GH_SCDEMO_BASEDIR%
 
 :END
 pause
